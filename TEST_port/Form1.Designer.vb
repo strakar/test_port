209c60f1fa9e146b5ad104120fb0acceb8ca6abc
unknown <Roman@romans.(none)>
2012-01-16 18:35:20 +0100

    ver1

---
+++ TEST_port/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.Button3 = New System.Windows.Forms.Button
+        Me.TextBox2 = New System.Windows.Forms.TextBox
+        Me.Button4 = New System.Windows.Forms.Button
+        Me.TextBox3 = New System.Windows.Forms.TextBox
+        Me.Button5 = New System.Windows.Forms.Button
+        Me.TextBox4 = New System.Windows.Forms.TextBox
+        Me.Button6 = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(27, 36)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(123, 36)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Timer1
+        '
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(27, 72)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(190, 221)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(123, 72)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 4
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(27, 111)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "Button4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(123, 111)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 6
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(387, 72)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 7
+        Me.Button5.Text = "Button5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(247, 72)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 8
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(27, 159)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 9
+        Me.Button6.Text = "Button6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(554, 266)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+
+End Class
---
+++ TEST_port/Form1.vb
@@ -0,0 +1,152 @@
+﻿Imports System.IO.Ports
+
+Public Class Form1
+    Dim port1 As New SerialPort()
+    Dim port2 As New SerialPort()
+    Dim port3 As New SerialPort()
+    Dim count As Integer
+
+
+    Public Function ctiport(ByVal port As String, ByVal co As String)
+        Dim odpoved As String
+        Dim comport As New SerialPort()
+        comport.PortName = port
+        If port = "COM1" Then
+            comport.BaudRate = 9600
+        Else
+            comport.BaudRate = 115200
+        End If
+        comport.Parity = 0
+        comport.DataBits = 8
+        comport.StopBits = 1
+        comport.ReadTimeout = 5000
+        comport.NewLine = Chr(13)
+        comport.DtrEnable = True
+        comport.Open()
+
+        comport.WriteLine(co + Chr(13))
+        odpoved = comport.ReadLine()
+
+        comport.Close()
+
+        Return (odpoved)
+
+    End Function
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Timer1.Enabled = True
+        Timer1.Interval = 2000
+
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Dim st As New Stopwatch()
+        Dim buffer As String
+        Dim pomoc, resp As Double
+
+
+        count = count + 1
+
+        st.Start()
+        port1.DtrEnable = True
+        buffer = "*B1TR1" + Chr(13)
+        port1.WriteLine(buffer)
+        buffer = port1.ReadLine()
+        TextBox1.Text = buffer
+        port1.DtrEnable = False
+        st.Stop()
+        pomoc = st.ElapsedMilliseconds
+        Button1.Text = CStr(st.ElapsedMilliseconds)
+        st.Reset()
+
+        st.Start()
+        port2.DtrEnable = True
+        port2.RtsEnable = True
+        buffer = "*B1TR1" + Chr(13)
+        port2.WriteLine(buffer)
+        buffer = port2.ReadLine()
+        buffer = Mid(buffer, 7, 4)
+        TextBox2.Text = buffer
+        port2.DtrEnable = False
+        st.Stop()
+        pomoc = pomoc + st.ElapsedMilliseconds
+        Button2.Text = CStr(st.ElapsedMilliseconds)
+
+        st.Reset()
+
+        st.Start()
+        port3.DtrEnable = True
+        buffer = "*B1MR0" + Chr(13)
+        port3.WriteLine(buffer)
+        buffer = port3.ReadLine()
+        TextBox3.Text = buffer
+        buffer = Mid(buffer, 10, 6) + ";" + Mid(buffer, 20, 6)
+        port3.DtrEnable = False
+        st.Stop()
+        TextBox3.Text = buffer
+        pomoc = pomoc + st.ElapsedMilliseconds
+        Button4.Text = CStr(st.ElapsedMilliseconds)
+
+        Button3.Text = CStr(count)
+
+        st.Reset()
+        st.Start()
+        Dim xmtBuf() As Byte = {&H2A, &H61, &H0, &H6, &H31, &H2, &H51, &H1, &HE9, &HD}
+        Dim rcvBuf(1024) As Byte
+        port2.DtrEnable = True
+        port2.Write(xmtBuf, 0, xmtBuf.Length)
+        port2.DtrEnable = False
+        port2.DtrEnable = True
+        port2.Read(rcvBuf, 0, rcvBuf.Length)
+        port2.DtrEnable = False
+        
+        TextBox4.Text = (rcvBuf(8) * 256 + rcvBuf(9) * 1) / 10
+        st.Stop()
+        pomoc = pomoc + st.ElapsedMilliseconds
+        Button5.Text = CStr(st.ElapsedMilliseconds)
+        Button6.Text = CStr(pomoc)
+        pomoc = 0
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Timer1.Enabled = False
+        Me.Close()
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        port1.PortName = "COM1"
+        port1.BaudRate = 9600
+        port1.Parity = 0
+        port1.DataBits = 8
+        port1.StopBits = 1
+        port1.ReadTimeout = 5000
+        port1.NewLine = Chr(13)
+        port1.Open()
+
+        port2.PortName = "COM4"
+        port2.BaudRate = 115200
+        port2.Parity = 0
+        port2.DataBits = 8
+        port2.StopBits = 1
+        port2.ReadTimeout = 5000
+        port2.NewLine = Chr(13)
+        port2.Open()
+
+        port3.PortName = "COM3"
+        port3.BaudRate = 115200
+        port3.Parity = 0
+        port3.DataBits = 8
+        port3.StopBits = 1
+        port3.ReadTimeout = 5000
+        port3.NewLine = Chr(13)
+        port3.Open()
+
+
+    End Sub
+End Class
